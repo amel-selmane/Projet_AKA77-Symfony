012d8cc27159cca0e316d9d4f512c37425c0f520
Mary <bonnetmarie@ymail.com>
2019-01-18 16:15:06 +0100

    galerie2

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -4,32 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\GalleryRepository;
 
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\GalleryRepository;
 
 class PublicController extends AbstractController
 {
 
-
     /**
-     * @Route("/galerie", name="galerie")
+     * @Route("/galerie", name="galerie", methods={"GET"})
      */
     public function gallery(GalleryRepository $galleryRepository) : Response
     {
-        /*
-        $listeGallery = $galleryRepository->findBy([], ["DatePublication" => "DESC"]);
-        dump($listeGallery);
-        return $this->render('public/gallery.html.twig', [
-            'gallerys' => $listeGallery,
-        ]);
-         */
+
+        return $this->render('public/galerie.html.twig');
 
     }
 
-
-
-
 }
-
-
-
