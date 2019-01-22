08d3b925631177dd85d2b9572c8d48a3744c9c66
4M3L <selmane.amel@gmail.com>
2019-01-22 14:42:42 +0100

    Modification formulaires

--- src/Form/GalleryType.php
+++ src/Form/GalleryType.php
@@ -6,24 +6,31 @@ use App\Entity\Gallery;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class GalleryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('imgName')
-            ->add('urlImgOriginal')
+            ->add('imgName', TextType::class, [
+                'label' => 'Nom de l\'image',
+                'attr' => ['placeholder' => "Nom de votre image"]
+            ])
+            ->add('urlImgOriginal', FileType::class, [
+                'label' => 'Chargez votre image (UPLOAD)',
+                'attr' => ['placeholder' => "Cliquez ici pour déposer votre image"]
+                ]);
             // ->add('imgLike')
-            // ->add('dateUpdate')
-            // ->add('idArtist')
-        ;
+            //->add('dateUpdate')
+            // ->add('idArtist');
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Gallery::class,
-        ]);
+        public function configureOptions(OptionsResolver $resolver)
+        {
+            $resolver->setDefaults([
+                'data_class' => Gallery::class,
+            ]);
+        }
     }
-}
