5b320e23ff70f292d3ecf3fd10dc1fe26c49d534
2m42hy <2m42hy@gmail.com>
2019-01-22 19:34:29 +0100

    try fix master branch

--- src/Commun/Config.php
+++ src/Commun/Config.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> e0f9e355002907c92ed33d2efd064dec161fb311
 <?php
 
 namespace App\Commun;
@@ -14,20 +10,4 @@ class Config
     }
 
 }
-<<<<<<< HEAD
-=======
-=======
-<?php
 
-namespace App\Commun;
-
-class Config
-{
-    function __construct()
-    {
-        dump($this);
-    }
-
-}
->>>>>>> 20dc5a8aa44b5a31c0b9b84dd843c1ec96eeefa1
->>>>>>> e0f9e355002907c92ed33d2efd064dec161fb311
--- src/Commun/Upload.php
+++ src/Commun/Upload.php
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> e0f9e355002907c92ed33d2efd064dec161fb311
-<?php
-
-namespace App\Commun;
-
-use App\Commun\Config;
-
-class Upload
-{
-
-    function __construct(Config $config)
-    {
-
-    }
-
-    public function gererUpload($objUploadedFile, $dossierCible)
-    {
-        dump($this);
-
-        $nomOrigine = $objUploadedFile->getClientOriginalName();
-        $extensionOrigine = strtolower($objUploadedFile->getClientOriginalExtension());
-        // $extensionOrigine = strtolower(pathinfo($nomOrigine, PATHINFO_EXTENSION));
-        dump($extensionOrigine);
-        $tabExtensionOK = ["jpg", "jpeg", "gif", "png"];
-        if (in_array($extensionOrigine, $tabExtensionOK)) {
-            // IMPORTANT: AJOUTER LA SECURITE SUR LA VERIF DU FICHIER (pas de .php)
-            // ON VA DEPLACER LE FICHIER UPLOADE DANS LE DOSSIER assets/upload/
-            // AJOUTER LE CHEMIN DANS config/services.yaml
-            // parameters:
-            //     monDossierUpload: '%kernel.project_dir%/public/assets/upload'
-            $objUploadedFile->move($dossierCible, $nomOrigine);
-            // ICI IL FAUDRAIT CREER LES MINIATURES
-        } else {
-            // ERREUR SUR L'UPLOAD
-            $nomOrigine = "";
-        }
-
-        return $nomOrigine;
-    }
-}
-<<<<<<< HEAD
-=======
-=======
 <?php
 
 namespace App\Commun;
@@ -86,5 +40,4 @@ class Upload
     }
 
 }
->>>>>>> 20dc5a8aa44b5a31c0b9b84dd843c1ec96eeefa1
->>>>>>> e0f9e355002907c92ed33d2efd064dec161fb311
+
