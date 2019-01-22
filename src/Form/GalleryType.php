55d1e86851e3e17d0f9880349c3fc66b3d2a9557
2m42hy <2m42hy@gmail.com>
2019-01-22 16:45:04 +0100

    befor sync xavier

--- src/Form/GalleryType.php
+++ src/Form/GalleryType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
